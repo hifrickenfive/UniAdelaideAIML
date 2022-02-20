1ed6cdc851d13485cd98ffa7de16b0f5ecb6e187
hifrickenfive <jasonalexanderchan@gmail.com>
2022-02-20 16:07:02 +1100

    Refactored 3layer CNN

---
+++ pytorch_assignment1.py
@@ -0,0 +1,183 @@
+# References to understand ML workflow
+# Pytorch: Quickstart: https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+# PyTorch Lightning: https://www.youtube.com/watch?v=OMDn66kM9Qc&ab_channel=PyTorchLightning
+# Krishna Ramesh: https://www.youtube.com/watch?v=ijaT8HuCtIY&ab_channel=KrishnaRamesh 
+
+# References to setup CNN
+# https://shap.readthedocs.io/en/latest/example_notebooks/image_examples/image_classification/PyTorch%20Deep%20Explainer%20MNIST%20example.html
+# https://towardsdatascience.com/mnist-handwritten-digits-classification-using-a-convolutional-neural-network-cnn-af5fafbc35e9
+# https://stackoverflow.com/questions/63754645/convert-conv2d-to-linear
+
+# Key concepts
+# Conv layer needs matrices as inputs while linear layers need flattened vectors.
+# Kernel size can't be greater than actual input size
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+from torchvision import datasets
+from torch import optim
+from torch import nn
+
+
+train_data = datasets.MNIST(
+    root = 'data',
+    train = True,                         
+    transform = ToTensor(), 
+    download = True,            
+)
+test_data = datasets.MNIST(
+    root = 'data', 
+    train = False, 
+    transform = ToTensor()
+)
+
+# Concatenate MNIST training and test set
+all_data = torch.utils.data.ConcatDataset([train_data, test_data])
+print(f'There is now {len(all_data)} data instances in all_data.')
+
+# Recut all_data: 70% training, 20% test, 10% validation.
+# Lesson leartn: can't naively slice pytorch data class. Use Subset method.
+idx_train = int(len(all_data)*0.7)
+idx_test = idx_train + int(len(all_data)*0.2)
+idx_validation = len(all_data)
+
+train_indices = list(range(0, idx_train))
+train_data_resliced = torch.utils.data.Subset(all_data, train_indices)
+print(f'There is now {len(train_data_resliced)} data instances in train_data_resliced (70% of all_data).')
+
+test_indices = list(range(idx_train, idx_test))
+test_data_resliced = torch.utils.data.Subset(all_data, test_indices)
+print(f'There is now {len(test_data_resliced)} data instances in test_data_resliced (20% of all_data).')
+
+validation_indices = list(range(idx_test, idx_validation))
+validation_data = torch.utils.data.Subset(all_data, validation_indices)
+print(f'There is now {len(validation_data)} data instances in validation_data (10% of all_data).')
+
+
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using {device} device")
+
+# Define models
+class NN_linear(nn.Module):
+    def __init__(self):
+        super(NeuralNetworkLinear, self).__init__()
+        self.flatten = nn.Flatten() # converts 2D arrays into one contiguous array.
+        self.linear_relu_stack = nn.Sequential( 
+            nn.Linear(28*28, 64), # args for CNN: input channels, output channels, kernel_size
+            nn.ReLU(), # an activation layer that introduces non-linearities to the linear model
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+class NN_CNN(nn.Module):
+    def __init__(self):
+        super(NN_CNN, self).__init__()
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(1, 10, kernel_size=5),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+
+            nn.Conv2d(10, 40, kernel_size=5),
+            nn.Dropout(),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+
+            nn.Conv2d(40, 200, kernel_size=3),
+            nn.Dropout(),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+        )
+        self.fc_layers = nn.Sequential(
+            nn.Linear(200, 50), #2nd layer creats 20x4x4 outputs. 3rd layer creates 40 outputs x 1 x 1 as f(kernel size, padding, output of layer 2)
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(50, 10),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        # Since you need matrices for conv. layers and for vectors linear layers, you have to take the matrix an flatten it, for example a matrix of shape (m, n) would become a vector (m*n, 1).
+        x = x.view(-1, x.size()[1] * x.size()[2] * x.size()[3]) # torch.Size([32, 20, 4, 4]). Batch size 32x of 20 output channels x 4 x 4
+        x = self.fc_layers(x)
+        return x
+
+
+def train(dataloader, model, loss_function, optimiser):
+    size = len(dataloader.dataset)
+    model.train()
+
+    for batch, (X, y) in enumerate(dataloader):
+        # X, y = X.to(device), y.to(device) # where x is image and y is label
+
+        # # Create matrix (why?)
+        # num_rows = X.size(0) # The batch size
+        # A = X.view(num_rows, -1) # Creates a matrix whose num_rows is batch size, num_columns = 28x28
+
+        # Compute predication error
+        logits = model(X) # Predict the result given this images
+        J = loss_function(logits, y)
+
+        # Backpropagration
+        model.zero_grad() # Equivalent to optimser.zero_grad() or params.grad._zero()
+        J.backward() # Accumulate the partial derivatives of J wrt params. Equivalent to params.grad._sum(dJ/dparams)
+        optimiser.step() # Step in the opposite direction of the gradient
+
+        # # Store loss
+        # losses.append(J.item())
+        
+        # Evaluate and store accuracy and store model
+        # accuracy = validate(model, validation_data)
+        # accuracies.append(accuracy)
+        # if accuracy > max_accuracy:
+        #     best_model = copy.deepcopy(model)
+        #     max_accuracy = accuracy
+
+        if batch % 100 == 0:
+            loss, current = J.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+# Load data into Pytorch loader
+train_loader = DataLoader(train_data_resliced, batch_size=32)
+validation_loader = DataLoader(validation_data, batch_size=32)
+test_loader = DataLoader(test_data_resliced, batch_size=32)
+
+# Setup model inputs
+# model = NN_linear()
+model = NN_CNN()
+optimiser = optim.SGD(model.parameters(), lr=1e-2)
+loss_function = nn.CrossEntropyLoss() # This defines how the optimiser changes the NN parameters
+
+# Train model
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_loader, model, loss_function, optimiser)
+    test(validation_loader, model, loss_function)
+print("Done!")
